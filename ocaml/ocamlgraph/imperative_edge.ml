30c2bdddc584fb0148c2b3512b13c0fb4a0a9695
Anwar Mamat <anwar@correctcomputation.com>
2020-10-16 13:42:48 -0400

    updates the edge label of the imperative graph

--- ocaml/ocamlgraph/imperative_edge.ml
+++ ocaml/ocamlgraph/imperative_edge.ml
@@ -1,34 +1,49 @@
 open Core
 open Graph
 
-module Node   = struct
-  type t = {id:int;label:string}
+module Node = struct
+
+  type nodeType = 
+    Branch_start
+  |Branch_end
+  |Flow
+  |Block
+
+  type t = {
+      id:int;
+      label:string;
+      mutable node_type:nodeType
+    }
   let hash = Hashtbl.hash
-  let equal = (=)
-  let create id label = {id; label}
+  let equal n1 n2 = n1.id = n2.id 
+  let create id lbl = {id = id; label = lbl; node_type = Block}
+  let setType node t = node.node_type <-t
+  let getType node = node.node_type
   let id n = n.id
-  let compare x y = compare x.id y.id
+  let compare x y = Int.compare (x.id) (y.id)
 end
 
 
 (* representation of an edge -- must be comparable *)
-module Edge = struct
+module Label = struct
   type t = ((int, Int.comparator_witness) Set.t) ref
   let to_string e = "[" ^ String.concat ~sep:"," (List.map ~f:(string_of_int) (Set.to_list (!e))) ^ "]"
   let compare e1 e2=
     let e1 = !e1 in
     let e2 = !e2 in
     Set.compare_direct e1 e2
-  let add e lbl = ref (Set.add (!e) lbl)
+  let add edge lbl = ref (Set.add (!edge) lbl)
+  let remove edge lbl = ref (Set.remove (!edge) lbl)
+  let mem edge lbl = Set.mem (!edge) lbl
   let equal = (=)
   let default = ref (Set.empty (module Int))
 end
 
-module G = Imperative.Digraph.ConcreteLabeled(Node)(Edge)
+module G = Imperative.Digraph.ConcreteLabeled(Node)(Label)
 
 module Dot = Graph.Graphviz.Dot(struct
                  include G (* use the graph module from above *)
-                 let edge_attributes (_, e, _) = [`Label (Edge.to_string e); `Color 4711]
+                 let edge_attributes (_, e, _) = [`Label (Label.to_string e); `Color 4711]
                  let default_edge_attributes _ = []
                  let get_subgraph _ = None
                  let vertex_attributes v = [`Shape `Box ;`Fontsize 24;`Label (G.V.label v).label]
@@ -42,16 +57,18 @@ let g_to_dot gr filename=
 
 let main () =
   let g = G.create () in
-  let v1 = G.V.create {id=1000;label="A"} in
-  let v2 = G.V.create {id=1001;label="B"} in
-  let e1 = Edge.default in
-  let e1 = Edge.add e1 1 in
-  let e1 = Edge.add e1 2 in
+  let v1 = Node.create 1000 "A" in
+  let v2 = Node.create 1001 "B" in
+  let e1 = Label.default in
+  let e1 = Label.add e1 1 in
+  let e1 = Label.add e1 2 in
   let e = G.E.create v1 e1  v2 in
   G.add_edge_e g e;
-  let e2 = Edge.default in
-  let e2 = Edge.add e2 3 in
-  let e2 = Edge.add e2 4 in
+  let e2 = Label.default in
+  let e2 = Label.add e2 3 in
+  let e2 = Label.add e2 4 in
+  let e2 = Label.add e2 5 in
+  let e2 = Label.remove e2 4 in
   let e = G.E.create v2 e2  v1 in
   G.add_edge_e g e;
   g_to_dot g "imperative_graph.dot"
