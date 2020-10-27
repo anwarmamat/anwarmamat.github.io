e181278002084f45a73af3449a2360dc3a1aa616
Anwar Mamat <anwar@correctcomputation.com>
2020-10-26 21:04:18 -0400

    misc

--- ocaml/ocamlgraph/imperative_edge.ml
+++ ocaml/ocamlgraph/imperative_edge.ml
@@ -2,7 +2,6 @@ open Core
 open Graph
 
 module Node = struct
-
   type nodeType = 
     Branch_start
   |Branch_end
@@ -32,11 +31,12 @@ module Label = struct
     let e1 = !e1 in
     let e2 = !e2 in
     Set.compare_direct e1 e2
-  let add edge lbl = ref (Set.add (!edge) lbl)
+  let add edge lbl = edge :=  (Set.add (!edge) lbl)
   let remove edge lbl = ref (Set.remove (!edge) lbl)
   let mem edge lbl = Set.mem (!edge) lbl
   let equal = (=)
   let default = ref (Set.empty (module Int))
+  let create () = ref (Set.empty (module Int))
 end
 
 module G = Imperative.Digraph.ConcreteLabeled(Node)(Label)
@@ -56,22 +56,40 @@ let g_to_dot gr filename=
   Dot.output_graph file gr
 
 let main () =
-  let g = G.create () in
+  
+  let g = G.create () in 
+
   let v1 = Node.create 1000 "A" in
   let v2 = Node.create 1001 "B" in
-  let e1 = Label.default in
-  let e1 = Label.add e1 1 in
-  let e1 = Label.add e1 2 in
-  let e = G.E.create v1 e1  v2 in
+  let l1 = Label.create () in
+  let _ = Label.add l1 1 in
+  let e = G.E.create v1 l1  v2 in
   G.add_edge_e g e;
-  let e2 = Label.default in
-  let e2 = Label.add e2 3 in
-  let e2 = Label.add e2 4 in
-  let e2 = Label.add e2 5 in
-  let e2 = Label.remove e2 4 in
-  let e = G.E.create v2 e2  v1 in
-  G.add_edge_e g e;
-  g_to_dot g "imperative_graph.dot"
+  Label.add (G.E.label e) 2;
+  Label.add (G.E.label e) 3;
+
+  let l2 = Label.create () in 
+  let _ = Label.add l2 4 in
+  let _ = Label.add l2 5 in
+  let _ = Label.remove l2 4 in
 
 
-let _ = main ();;
+  let e3 = G.E.create v2 l2 v1 in
+  G.add_edge_e g e3;
+
+  let l3 = Label.create () in 
+  let _ = Label.add l3 10 in
+  let _ = Label.add l3 20 in
+  let _ = Label.add l3 30 in
+
+
+  let e4 = G.E.create v2 l3 v1 in
+  G.add_edge_e g e4;
+
+
+  g_to_dot g "imperative_graph.dot";
+  let edge = G.find_edge g v1 v2 in 
+  Printf.printf "Edge: (%d,%s,%d)\n" (Node.id (G.E.src edge)) (Label.to_string (G.E.label edge)) (Node.id (G.E.dst edge)) ;
+  Printf.printf "e3=e4: %d\n" (G.E.compare e3 e4)
+
+ let _ = main ();;
